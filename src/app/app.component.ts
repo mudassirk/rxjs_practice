fb30966119a08cf6550f04fe402b1dc365a54d57
mudassir <mudassirkmca@gmail.com>
2024-08-21 15:38:45 +0530

    Initial project code

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { rejects } from 'node:assert';
+import { error } from 'node:console';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title = 'rxjs_practice';
+
+  
+
+  ngOnInit(): void {
+    let promise  = new Promise((resolve, reject) =>{
+      setTimeout(()=>{
+        resolve('Promise Resolved')
+      },3000)
+    })
+
+async function getData(){
+  let proValue = await promise;
+  console.log(proValue)
+}
+
+getData();
+
+  }
+}
