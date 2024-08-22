80441f044611bbe65063733b4c5f4794546928e6
mudassir <mudassirkmca@gmail.com>
2024-08-22 16:25:00 +0530

    New bugs fixes and the priority functionality has been done on the cromp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { rejects } from 'node:assert';
-import { error } from 'node:console';
+import { response } from 'express';
+import { fromEvent, interval, of, Subscription, timer } from 'rxjs';
+import internal from 'stream';
 
 @Component({
   selector: 'app-root',
@@ -10,24 +11,30 @@ import { error } from 'node:console';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  title = 'rxjs_practice';
+export class AppComponent implements OnInit, AfterViewInit{
+  title: any;
+subscriptions : Subscription | undefined;
+   @ViewChild('addBtn') addBtn!:ElementRef;
+  result:any ='';
+  ngOnInit(): void {  
+  //   const timers = timer(5000, 2000);
+  //  this.subscriptions =  timers.subscribe(re =>{
+  //     console.log(re)
 
-  
+  //     if(re > 5){
+  //       this.subscriptions?.unsubscribe()
+  //     }
+  //   })
 
-  ngOnInit(): void {
-    let promise  = new Promise((resolve, reject) =>{
-      setTimeout(()=>{
-        resolve('Promise Resolved')
-      },3000)
+    const offStream =  of('Angular', 'Javascript', 'mongoDB');
+
+    offStream.subscribe(res =>{
+           console.log(res)
     })
-//for asysn and await
-async function getData(){
-  let proValue = await promise;
-  console.log(proValue)
-}
 
-getData();
+  }
 
+  ngAfterViewInit(): void {
+    
   }
 }
